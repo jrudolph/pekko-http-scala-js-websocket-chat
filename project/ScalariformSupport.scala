9bb0dd0a53b5a19bd197713d4cb7cc4e45d40f10
Nicolas Rémond <nremond+git@gmail.com>
2017-01-11 11:05:32 +0100

    Bump sbt-scalariform 1.6.0

--- project/ScalariformSupport.scala
+++ project/ScalariformSupport.scala
@@ -5,16 +5,18 @@ import com.typesafe.sbt.SbtScalariform.ScalariformKeys
 
 object ScalariformSupport {
   lazy val formatSettings = SbtScalariform.scalariformSettings ++ Seq(
-    ScalariformKeys.preferences in Compile := formattingPreferences,
-    ScalariformKeys.preferences in Test    := formattingPreferences
+    ScalariformKeys.preferences in Compile := formattingPreferences.value,
+    ScalariformKeys.preferences in Test    := formattingPreferences.value
   )
 
   import scalariform.formatter.preferences._
-  def formattingPreferences =
-    FormattingPreferences()
+  def formattingPreferences = Def.setting {
+    ScalariformKeys.preferences.value
       .setPreference(RewriteArrowSymbols, true)
       .setPreference(AlignParameters, true)
       .setPreference(AlignSingleLineCaseStatements, true)
       .setPreference(DoubleIndentClassDeclaration, true)
+      .setPreference(RewriteArrowSymbols, false)
+  }
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1")
+addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.6.0")
 
 addSbtPlugin("io.spray" % "sbt-revolver" % "0.8.0")
 
